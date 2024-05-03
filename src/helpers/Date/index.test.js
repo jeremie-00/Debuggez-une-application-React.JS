03c51088f63c7979d73f233bf3c3ed54076c5952
jeremie <jeremie.herault95@orange.fr>
2024-05-03 11:49:20 +0200

    update test helper date

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -2,14 +2,19 @@
  * 
  */
 
+import { getMonth } from "../../helpers/Date";
+
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
-        it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
+        it("the function returns 'janvier' for the date 2022-01-01", () => {
+            const result = getMonth(new Date("2022-01-01"));
+            expect(result).toBe("janvier");
         });
-        it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
+        it("the function returns 'juillet' for the date 2022-07-08", () => {
+            const result = getMonth(new Date("2022-07-08"));
+            expect(result).toBe("juillet");
         });
     });
-})
+});
+
 
